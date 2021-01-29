b88a8460880d8652c5b36b0b8d39f32cb55f5acb
mati <you@example.com>
2021-01-29 16:09:10 +0100

    classe29012021

---
+++ ahorcado/src/ahorcado/Ahorcado.java
@@ -0,0 +1,289 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ahorcado;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author mati
+ */
+public class Ahorcado {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String paraula = ficar_paraula();
+        for (int i=0; i < 50; i++)
+
+{
+
+System.out.println("");
+
+}
+        char[] lletres = new char[paraula.length()];
+        int vides = 7;
+        rellenarArraysGuion(lletres);
+
+        do {
+
+            mostrar_lletres(lletres);
+            char letra = pedirletra();
+
+            System.out.println("Has escrito:" + letra);
+
+            boolean trobarlletra = encontrarLetra(paraula, lletres, letra);
+
+            System.out.println(trobarlletra);
+
+            if (!trobarlletra) {
+                vides--;
+                munyeco(vides);
+                //System.out.println("Te quedan " + vides);
+            }
+            else
+            {
+                System.out.println("has acertado letra");
+            }
+
+        } while (vides > 0 && has_ganado(lletres) == false);
+
+    }
+
+    private static String ficar_paraula() {
+        Scanner sc = new Scanner(System.in);
+        String paraula;
+        do {
+            System.out.println("Fica una paraula alemhys 3 lletres");
+            paraula = sc.next();//elimina ja els espais
+        } while (paraula.length() < 3);
+        return paraula.toUpperCase();
+    }
+
+    private static void rellenarArraysGuion(char[] lletres) {
+        for (int i = 0; i < lletres.length; i++) {
+            lletres[i] = '_';
+        }
+
+        //Arrays.fill(lletres, '_');
+    }
+
+    private static void mostrar_lletres(char[] lletres) {
+        for (int i = 0; i < lletres.length; i++) {
+            System.out.print(lletres[i] + " ");
+        }
+        System.out.println("");
+    }
+
+    private static char pedirletra() {
+        Scanner sc = new Scanner(System.in);
+        char letra;
+
+        System.out.println("Dime una letra(en mayuscula)");
+        letra = sc.next().charAt(0);
+
+        return letra;
+    }
+
+    private static boolean encontrarLetra(String paraula, char[] lletres, char letra) {
+        boolean check = false;
+        for (int i = 0; i < lletres.length; i++) {
+            if (paraula.charAt(i) == letra) {
+                lletres[i] = letra;
+                check = true;
+            }
+        }
+
+        return check;
+    }
+
+    private static boolean has_ganado(char[] lletres) {
+        
+        for (int i = 0; i < lletres.length; i++) {
+            if (lletres[i]=='_') {
+                return false;
+            }          
+        }
+        return true;
+    }
+
+
+     private static void munyeco(int intentos) {
+
+        switch (intentos) {
+            case 6:
+                System.out.println(" ---------------------");
+                for (int j = 0; j < 15; j++) {
+                    System.out.println(" |");
+                }
+                System.out.println("__________");
+                break;
+            case 5:
+                System.out.println(" ---------------------");
+                System.out.println(" |                     |");
+                System.out.println(" |                     |");
+                System.out.println(" |                  -------");
+                System.out.println(" |                 | -  -  |");
+                System.out.println(" |                 |   o   |");
+                System.out.println(" |                  -------");
+                for (int j = 0; j < 10; j++) {
+                    System.out.println(" |");
+                }
+                System.out.println("__________");
+                break;
+            case 4:
+                System.out.println(" ---------------------");
+                System.out.println(" |                     |");
+                System.out.println(" |                     |");
+                System.out.println(" |                  -------");
+                System.out.println(" |                 | -  -  |");
+                System.out.println(" |                 |   o   |");
+                System.out.println(" |                  -------");
+                System.out.println(" |                     |   ");
+                System.out.println(" |                     |   ");
+                System.out.println(" |                     |   ");
+                System.out.println(" |                     |   ");
+                System.out.println(" |                     |   ");
+                for (int j = 0; j < 5; j++) {
+                    System.out.println(" |");
+                }
+                System.out.println("__________");
+                break;
+            case 3:
+                System.out.println(" ---------------------");
+                System.out.println(" |                     |");
+                System.out.println(" |                     |");
+                System.out.println(" |                  -------");
+                System.out.println(" |                 | -  -  |");
+                System.out.println(" |                 |   o   |");
+                System.out.println(" |                  -------");
+                System.out.println(" |                     |   ");
+                System.out.println(" |                   / |   ");
+                System.out.println(" |                 /   |   ");
+                System.out.println(" |                /    |   ");
+                System.out.println(" |                     |   ");
+                for (int j = 0; j < 5; j++) {
+                    System.out.println(" |");
+                }
+                System.out.println("__________");
+
+                break;
+            case 2:
+                System.out.println("                     ,____\n"
+                        + "                      |---.\\\n"
+                        + "              ___     |    `\n"
+                        + "             / .-\\  ./=)\n"
+                        + "            |  |\"|_/\\/|\n"
+                        + "            ;  |-;| /_|\n"
+                        + "           / \\_| |/ \\ |\n"
+                        + "          /      \\/\\( |\n"
+                        + "          |   /  |` ) |\n"
+                        + "          /   \\ _/    |\n"
+                        + "         /--._/  \\    |\n"
+                        + "         `/|)    |    /\n"
+                        + "           /     |   |\n"
+                        + "         .'      |   |\n"
+                        + "  jgs   /         \\  |\n"
+                        + "       (_.-.__.__./  /");
+                break;
+            case 1:
+                System.out.println("               ...\n"
+                        + "             ;::::;\n"
+                        + "           ;::::; :;\n"
+                        + "         ;:::::'   :;\n"
+                        + "        ;:::::;     ;.\n"
+                        + "       ,:::::'       ;           OOO\\\n"
+                        + "       ::::::;       ;          OOOOO\\\n"
+                        + "       ;:::::;       ;         OOOOOOOO\n"
+                        + "      ,;::::::;     ;'         / OOOOOOO\n"
+                        + "    ;:::::::::`. ,,,;.        /  / DOOOOOO\n"
+                        + "  .';:::::::::::::::::;,     /  /     DOOOO\n"
+                        + " ,::::::;::::::;;;;::::;,   /  /        DOOO\n"
+                        + ";`::::::`'::::::;;;::::: ,#/  /          DOOO\n"
+                        + ":`:::::::`;::::::;;::: ;::#  /            DOOO\n"
+                        + "::`:::::::`;:::::::: ;::::# /              DOO\n"
+                        + "`:`:::::::`;:::::: ;::::::#/               DOO\n"
+                        + " :::`:::::::`;; ;:::::::::##                OO\n"
+                        + " ::::`:::::::`;::::::::;:::#                OO\n"
+                        + " `:::::`::::::::::::;'`:;::#                O\n"
+                        + "  `:::::`::::::::;' /  / `:#\n"
+                        + "   ::::::`:::::;'  /  /   `#");
+                break;
+            case 0:
+                System.out.println("░░░░░░░░░░░░░░░░▓██████▓▓▓░░░░░░░░░░░░░░░\n"
+                        + "░░░░░░░░░░░░░█████▓▓█████████▓░░░░░░░░░░░\n"
+                        + "░░░░░░░░░░█████▓░░▓█████████████░░░░░░░░░\n"
+                        + "░░░░░░░░▓███▓░░░▓█████████████████░░░░░░░\n"
+                        + "░░░░░░░███▓░░░░░███████████████████▓░░░░░\n"
+                        + "░░░░░░███░░░░░░██████████████████████░░░░\n"
+                        + "░░░░░███░░░░░░░███████████████████████░░░\n"
+                        + "░░░░███░░░░░░░░███████░░░░██████████▓█▓░░\n"
+                        + "░░░███▓░░░░░░░░███████░░░░▓██████████▓█░░\n"
+                        + "░░▓███░░░░░░░░░░██████▓░░▓███████████▓██░\n"
+                        + "░░████░░░░░░░░░░▓████████████████████▓▓█░\n"
+                        + "░▓█░█▓░░░░░░░░░░░░████████████████████░██\n"
+                        + "░██░█░░░░░░░░░░░░░░▓██████████████████░██\n"
+                        + "░█▓░█░░░░░░░░░░░░░░░░░▓███████████████░▓█\n"
+                        + "▓█▓░█▓░░░░░░░░░░░░░░░░░░██████████████░░█\n"
+                        + "██░░██░░░░░░░░░░░▓▓░░░░░░▓████████████░░█\n"
+                        + "██░▓░█░░░░░░░░░░████▓░░░░░███████████▓░░█\n"
+                        + "██░█░██░░░░░░░░▓█████░░░░░░██████████░░▓█\n"
+                        + "██░▓█░██░░░░░░░░████▓░░░░░░█████████░░▓▓█\n"
+                        + "▓█░░█░░█▓░░░░░░░░▓▓░░░░░░░░████████▓░░▓██\n"
+                        + "░█░░█▓░▓██░░░░░░░░░░░░░░░░░███████▓░░█▓█▓\n"
+                        + "░██░███████▓░░░░░░░░░░░░░░██████████▓█▓█░\n"
+                        + "░▓█░██▓░░░▓███░░░░░░░░░░▓██████▓░░░▓██▓█░\n"
+                        + "░░███▓░░░░░░░███████████████▓░░░░░░░░██▓░\n"
+                        + "░░░██░░▓▓█▓▓▓░░░▓████████▓░░░░▓▓█▓▓░░██░░\n"
+                        + "░░░▓█░████████▓░░░░░░░░░░░░▓████████░▓█░░\n"
+                        + "░░░█▓▓███████████░░░░░░░░████████████░█▓░\n"
+                        + "░░░█░█████████████░░░░░░█████████████░██░\n"
+                        + "░░▓█░▓████████████░░░░░░█████████████░░█░\n"
+                        + "░░▓█░▓▓███████████░░░░░░███████████▓▓░░█░\n"
+                        + "░░▓█░░░▓█████████░░░░░░░░█████████▓░░░░█░\n"
+                        + "░░░█▓░░░████████░░░░░░░░░░████████░░░░██░\n"
+                        + "░░░██░░░░░█████░░░░████░░░░█████▓░░░░░█▓░\n"
+                        + "░░░░██░░░░░░░░░░░░██████░░░░░░░░░░▓░░██░░\n"
+                        + "░░░░▓████▓░░░░░░░░███▓██▓░░░░░░░░█████░░░\n"
+                        + "░░░░░▓█▓████▓░░░░░██░▓▓██░░░░▓████░██░░░░\n"
+                        + "░░░░░░░░▓█▓██░▓░░░██▓▓▓██░░█▓█████░░░░░░░\n"
+                        + "░░░░░░░░▓█░███▓░░░▓▓▓░▓░░░░░█▓██▓█░░░░░░░\n"
+                        + "░░░░░░░░▓█░██▓░░▓░░░░░░░░░▓░███▓▓█░░░░░░░\n"
+                        + "░░░░░░░░▓█░███▓███▓░░░░░▓███▓██░▓█░░░░░░░\n"
+                        + "░░░░░░░░██░░██░▓░█████████░▓▓█▓░▓█░░░░░░░\n"
+                        + "░░░░░░░░▓█░░▓██▓▓░░▓░█░█░▓░▓██░░░█░░░░░░░\n"
+                        + "░░░░░░░░░█▓░░████░▓░░▓░░▓▓███░░░██░░░░░░░\n"
+                        + "░░░░░░░░░▓█▓░░████████▓█████░░░██▓░░░░░░░\n"
+                        + "░░░░░░░░░░░██░░▓▓▓▓▓▓▓▓▓▓▓█░░░██░░░░░░░░░\n"
+                        + "░░░░░░░░░░░░██░░▓█████▓██▓░░▓██░░░░░░░░░░\n"
+                        + "░░░░░░░░░░░░░██░░░░░▓▓░░░░░███░░░░░░░░░░░\n"
+                        + "░░░░░░░░░░░░░░██░░░░░░░░░░██▓░░░░░░░░░░░░\n"
+                        + "░░░░░░░░░░░░░░▓██░░░░░░░░██░░░░░░░░░░░░░░\n"
+                        + "░░░░░░░░░░░░░░░░██████████░░░░░░░░░░░░░░░");
+
+                System.out.println(ANSI_RED + "GAME OVER" + ANSI_RESET);
+                break;
+            default:
+                throw new AssertionError();
+        }
+
+    }
+    
+    
+    
+    
+}
---
+++ alexaokgooglesiri/src/alexaokgooglesiri/Alexaokgooglesiri.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alexaokgooglesiri;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author mati
+ */
+public class Alexaokgooglesiri {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nombre = pide_nombre();
+        boolean no_terminar = false;
+        do {
+            String instruccion = pide_instruccion();
+            //comprobar alexa esta al principio
+
+            if (instruccion.startsWith("ALEXA")) {
+
+                String orden = instruccion.substring(6);
+                
+                /* System.out.println("." + orden + ".");
+            orden= orden.trim();//eliminar espacios delante y atras elimino alexa instruccion elimino espacio delante */
+                System.out.println("." + orden + ".");
+                no_terminar = instrucciones(orden, nombre);
+            } else {
+                System.out.println("no has dicho alexa");
+            }
+        } while (no_terminar == true);
+        //
+
+    }
+
+    private static String pide_nombre() {
+        Scanner sc = new Scanner(System.in);
+        String paraula;
+        do {
+            System.out.println("Com et dius?");
+            paraula = sc.nextLine();//elimina ja els espais
+        } while (paraula.isEmpty());
+        return paraula;
+    }
+
+    private static String pide_instruccion() {
+        Scanner sc = new Scanner(System.in);
+        String instruccion;
+        do {
+            System.out.println("Esperando instruccion");
+            instruccion = sc.nextLine();//elimina ja els espais
+        } while (instruccion.isEmpty());
+        return instruccion.toUpperCase();
+    }
+
+    private static boolean es_alexa(String instruccion) {
+        String alexa = instruccion.substring(0, 4);
+        //instruccion.startsWith("ALEXA")
+        //if(alexa.indexOf("ALEXA")==0)
+        if (alexa.equals("ALEXA")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static boolean instrucciones(String orden, String nombre) {
+        //escoger la instruccio
+        if (orden.contains("BUENOS DIAS")) {
+            System.out.println("Buenos dias " + nombre);
+            return true;
+        }
+        if (orden.contains("BUENAS NOCHES")) {
+            System.out.println("BuenAS NOCHES " + nombre);
+            return false;
+        }
+        if (orden.contains("CANTA UNA CANCION")) {
+            cantar();
+        }
+        if (orden.contains("HABLA CON LA")) {
+            habla(orden);
+        }
+        if (orden.contains("TABLAS DE MULTIPLICAR")){
+            multiplicar();
+        }
+
+        return true;
+    }
+
+    private static void cantar() {
+        for (int i = 2; i <= 10; i++) {
+            System.out.println(i + " elefantes se balanceaban...");
+        }
+    }
+
+    private static void habla(String orden) {
+        char letra = orden.charAt(13);
+        System.out.println(letra);
+        String fraseFound = orden.substring(20);
+        System.out.println(fraseFound);
+        
+        fraseFound = fraseFound.replace('A', letra);
+        fraseFound = fraseFound.replace('E', letra);
+        fraseFound = fraseFound.replace('I', letra);
+        fraseFound = fraseFound.replace('O', letra);
+        fraseFound = fraseFound.replace('U', letra);
+        System.out.println("frase cambiada");
+        System.out.println(fraseFound);
+/*
+        for (int i = 0; i < fraseFound.length(); i++) {
+            if ('A' == fraseFound.charAt(i)) {
+                fraseFound.charAt(i) = letra;
+            }
+        }
+*/
+    }
+
+    private static void multiplicar() {
+        Scanner sc = new Scanner(System.in);
+        Random r = new Random();
+        int v1,v2;
+        
+        v1 = r.nextInt(10)+1;
+        v2 = r.nextInt(10)+1;
+        
+        long inicio = System.currentTimeMillis(); /*temps en ms*/
+        System.out.println("Cuanto es "+v1+" x "+v2);
+        int respuesta = sc.nextInt();
+        long fin = System.currentTimeMillis(); /*temps en ms*/
+        
+        long tiempo = (fin-inicio)/1000;
+        
+        if (respuesta==(v1*v2) && tiempo<5)
+        {
+            System.out.println("Piruleta para ti");
+        }
+        else
+        {
+            System.out.println("Piensa mas");
+        }
+    }
+
+}
