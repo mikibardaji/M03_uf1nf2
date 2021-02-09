f12c84f6608f43889b09a5a3e2598860f9bd6330
MiquelAngel <mabarda@hotmail.com>
2021-02-09 15:15:22 +0100

    20210209

--- Arrays_bidimensionales/uf1nf2_ex5bidimensional/src/uf1nf2_ex5bidimensional/Uf1nf2_ex5bidimensional.java
+++ Arrays_bidimensionales/uf1nf2_ex5bidimensional/src/uf1nf2_ex5bidimensional/Uf1nf2_ex5bidimensional.java
@@ -5,6 +5,8 @@
  */
 package uf1nf2_ex5bidimensional;
 
+import java.util.Scanner;
+
 /**
  *
  * Necesitamos crear un programa para 
@@ -26,7 +28,74 @@ public class Uf1nf2_ex5bidimensional {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Cuantos trabajadores tienes?");
+        int num_trabajadores = sc.nextInt();
+        int[][] sueldos = new int[2][num_trabajadores];
+        int homes=0, dones=0;
+        int[] homes_dones = new int[2];
+        int sexe, sueldo;
+        //inclou dades al array pero ordenades per sexe
+        incloure_datos(sueldos,homes_dones,num_trabajadores);
+        mostrar_array(sueldos);
+        calcular_media(sueldos,homes_dones);
+        
+    }
+
+    private static void incloure_datos(int[][] sueldos, int[] homes_dones, int num_trabajadores) {
+        Scanner sc = new Scanner(System.in);
+        int sexe,sueldo;
+        for (int i = 0; i < num_trabajadores; i++) {
+            System.out.println("sexo del trabajador " + (i+1));
+            sexe = sc.nextInt();
+            System.out.println("sueldo trabajador " + (i+1));
+            sueldo = sc.nextInt();
+            //incloure array
+            if (sexe==1) //dones
+            {
+               sueldos[sexe][homes_dones[sexe]] = sueldo;
+               homes_dones[sexe]++;//aumento la posicio 1
+            }
+            else
+            {
+                sueldos[sexe][homes_dones[sexe]] = sueldo;
+               homes_dones[sexe]++;
+            }
+        }
+    }
+
+    private static void mostrar_array(int[][] sueldos) {
+        for (int i = 0; i < sueldos.length; i++) {
+            for (int j = 0; j < sueldos[0].length; j++) {
+                System.out.printf("%4d",sueldos[i][j]);
+            }
+            System.out.println("");
+        }
+        
+    }
+
+    private static void calcular_array(int[][] sueldos, int[] homes_dones) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void calcular_media(int[][] sueldos, int[] homes_dones) {
+          int hombres= homes_dones[0];
+          int dones = homes_dones[1];
+          
+          int sueldo_h=0, sueldo_m=0;
+          //hombres fila 0 es fija
+          for (int i = 0; i < hombres; i++) {
+            sueldo_h = sueldo_h + sueldos[0][i];
+        }
+          double media_h = (double) sueldo_h/hombres;
+          System.out.println("hombres cobran: " + media_h);
+          
+           for (int i = 0; i < dones; i++) {
+            sueldo_m = sueldo_m + sueldos[1][i];
+        }
+          double media_d = (double) sueldo_m/dones;
+          System.out.println("mujeres cobran: " + media_d);
+          
     }
     
 }
--- Arrays_bidimensionales/uf1nf2_exbidimensionalconecta/src/uf1nf2_exbidimensionalconecta/Uf1nf2_exbidimensionalconecta.java
+++ Arrays_bidimensionales/uf1nf2_exbidimensionalconecta/src/uf1nf2_exbidimensionalconecta/Uf1nf2_exbidimensionalconecta.java
@@ -5,6 +5,8 @@
  */
 package uf1nf2_exbidimensionalconecta;
 
+import java.util.Scanner;
+
 /**
  *
  * @author mati
@@ -16,19 +18,57 @@ public class Uf1nf2_exbidimensionalconecta {
      */
     public static void main(String[] args) {
         int filas=7;
-        int columnas=8;
-        int[][] tauler = new int[7][8];
+        int columnas=7;
+        Scanner sc = new Scanner(System.in);
+        int[][] tauler = new int[filas][columnas];
+        String[] usuaris = new String[2]; //per guardar els noms dels 2 usuaris
         //Demanar noms cada usuari
+        for (int i = 0; i < usuaris.length; i++) {
+           usuaris[i]= pedir_nombre();    
+        }
+       
+        
+        int turno=triar_inici();
+        //System.out.println("turno" + turno);
+        
+        
         //-Triar_inici random escoge entre 1 y 2
-        tauler[1][2]=1;
-        tauler[2][2]=2;
+        
         mostrar_tauler(tauler);
+        do{
+            if(turno==1)
+            {
+                turno=0;
+            }
+            else
+            {
+                turno=1;
+            }
+            //ir cambiando turno
+            //turno = (turno+1)%2;
+            System.out.println("turno de " + usuaris[turno]);
+            System.out.println("En que columnas quieres colocar ficha");
+            int columna = sc.nextInt();
+
+            tirada(tauler,columna,turno);
+            mostrar_tauler(tauler);
+   
+        }while(!tauler_ple(tauler) && !has_ganado(tauler,turno));
+        
+        if(tauler_ple(tauler)==false)
+        {
+            System.out.println("ganador " + usuaris[turno]);
+        }
+        else
+        {
+            System.out.println("has llenado el tablero empate");
+        }
     }
 
     private static void mostrar_tauler(int[][] tauler) {
         for (int fila = 0; fila < tauler.length; fila++) {
             for (int columna = 0; columna < tauler[fila].length; columna++) {
-         System.out.print(Mostrar_casella_usuari(fila,columna,tauler)+"|");
+               System.out.print(Mostrar_casella_usuari(fila,columna,tauler)+"|");
                 
             }
             System.out.println("");
@@ -45,5 +85,109 @@ public class Uf1nf2_exbidimensionalconecta {
             return 'X';
         else
             return 'O';
+    }
+
+    private static String pedir_nombre() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("como te llamas?");
+        String nombre = sc.nextLine();
+        return nombre;
+    }
+
+    private static int triar_inici() {
+        int num = (int) (Math.random()*2) ;
+        return num;
+    }
+
+    private static void tirada(int[][] tauler, int columna, int turno) {
+        //buscar la primera posicion vacia o sea que es 0
+        //en la columna elegida
+        //cuando la encuentre pone su fica (1 o 2)
+        //turno 0 coloca ficha con un 1
+        //turno 1 coloca ficha con un 2
+        int i;
+        for ( i = tauler.length-1; i >= 0; i--) {
+            if(tauler[i][columna]==0)
+            {//es que esta vacia y puedo colocar mi ficha
+                tauler[i][columna]=(turno+1);
+                break; //no recorrer mas el bucle por que 
+                //ya he colocado la ficha
+            }
+            
+        }
+        //columna llena o has colocado
+        if(i<0)
+        {
+            System.out.println("columna llena");
+        }
+        //si no entro en el if es que he colocado ficha
+        
+        
+    }
+
+    private static boolean tauler_ple(int[][] tauler) {
+        for (int i = 0; i < tauler.length; i++) {
+            for (int j = 0; j < tauler[0].length; j++) {
+                if(tauler[i][j]==0)
+                {
+                    return false; //hay una vacia
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean has_ganado(int[][] tauler, int turno) {
+        //a partir turno tengo que mirar si en horizontal hay 4 en linea
+        //turno es 0 o 1
+        //jugador 0 las fichas son el 1
+        //jugador 1 las fichas son el 2
+        int ficha = turno+1; //con esto convierto a la ficha
+        //variable ficha va a tener el valor del jugador que toque
+        int contar=0;
+        
+        //horizontal
+        for (int filas = 0; filas < tauler.length; filas++) {
+            for (int columnas = 0; columnas < tauler[0].length; columnas++) {
+                if(tauler[filas][columnas]==ficha)
+                {
+                    contar++;
+                    if(contar==4)
+                    {
+                        return true; //he ganado
+                    }
+                }
+                else
+                {
+                    contar=0; //inicializo por que no estan seguidas
+                }
+                
+            }
+            //he terminado la fila en horizonta
+            contar=0;//terminado linea
+        }
+        
+        for (int columnas = 0; columnas < tauler.length; columnas++) {
+            for (int filas = 0; filas < tauler[0].length; filas++) {
+                if(tauler[filas][columnas]==ficha)
+                {
+                    contar++;
+                    if(contar==4)
+                    {
+                        return true; //he ganado
+                    }
+                }
+                else
+                {
+                    contar=0; //inicializo por que no estan seguidas
+                }
+                
+            }
+            //he terminado la fila en horizonta
+            contar=0;//terminado linea
+        }
+        
+        return false; //no he ganado
+        
     }
 }
