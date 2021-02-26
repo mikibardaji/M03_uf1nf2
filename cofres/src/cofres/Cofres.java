a85c8d5803d81826ebbe0c9e24cad287bd0528c1
mati <you@example.com>
2021-02-26 16:14:16 +0100

    Cofres2

--- cofres/src/cofres/Cofres.java
+++ cofres/src/cofres/Cofres.java
@@ -5,6 +5,8 @@
  */
 package cofres;
 
+import java.util.Arrays;
+
 /**
  *
  * @author mati
@@ -17,7 +19,7 @@ public class Cofres {
     public static void main(String[] args) {
         Carta[] disponibles = new Carta[15];//todas las del juego
         Carta[] tuyas = new Carta[disponibles.length];
-        
+        int num_cartas_tuyas = 0;
         cargar_cartas_disponibles(disponibles);
         
         
@@ -28,12 +30,23 @@ public class Cofres {
     }
 
     private static void cargar_cartas_disponibles(Carta[] disponibles) {
+        //Arrays.fill(disponibles, new Carta());
         disponibles[0] = new Carta();
         disponibles[0].nombre = "Pekka";
         disponibles[0].elixir = 7;
+        //disponibles[0].level no hace falta
+        //disponibles[0].puntos_carta no hace
         disponibles[1] = new Carta();
         disponibles[1].nombre = "Mini Pekka";
         disponibles[1].elixir = 4;
+        
+        disponibles[2] = new Carta();
+        disponibles[2].nombre = "Leñador";
+        disponibles[2].elixir = 4;
+        
+        disponibles[3] = new Carta();
+        disponibles[3].nombre = "Globo Bombastico";
+        disponibles[3].elixir = 4;
     }
     
 }
